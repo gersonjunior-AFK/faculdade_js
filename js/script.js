14583c904189f1f667043d2abc9157be61881897
Gerson da Silva Carvalho Júnior <gerson61752726@edu.df.senac.br>
2026-08-28 19:56:40 -0300

    Versao inicial do projeto

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function media() {
+let num1 = Number(prompt("Primeiro número"));
+let num2 = Number(prompt("Segundo número"));
+alert(`Média: ${(num1 + num2) / 2}`);
+}
---
+++ js/script2.js
@@ -0,0 +1,10 @@
+function cadastrar() {
+let num1 = Number(prompt("Digite um número:"));
+
+if (num1 % 2 == 0){
+    alert("seu numero e par:")
+}else {
+    alert("seu numero e impar")
+}
+
+}
